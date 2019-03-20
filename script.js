e60f3c5fccc051031252684186f4de92fa22a6ac
dialloconkry <43544103+dialloconkry@users.noreply.github.com>
2019-03-20 10:13:48 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$("button").click(function(){
+   var message= $(".name").val();
+  var result= $(".age").val() - 2050;
+   var r=$(".3").val() + 2015;
+  $(".line-17").text("hello" + message + result + r);
+ 
+  
+    
+    
+});  
+ 
+
+
